7e3effc15b6c8d44cf28ffe03cdb31d852f55323
Kevin Kriek <kevinkriek@gmail.com>
2017-11-25 15:41:24 +0100

    added filter functionality

--- js/app.js
+++ js/app.js
@@ -52,13 +52,22 @@ var Location = function(data) {
         icon: defaultIcon
     });
 
-    self.marker.setMap(map);
-    bounds.extend(self.marker.position);
-    map.fitBounds(bounds);
+    self.filterMarkers = ko.computed(function () {
+        // Set markers and adjust bounds
+        if(self.visible() === true) {
+            self.marker.setMap(map);
+            bounds.extend(self.marker.position);
+            map.fitBounds(bounds);
+        } else {
+            self.marker.setMap(null);
+        }
+    });
 
     // Create an onclick even to open an indowindow at each marker
     this.marker.addListener('click', function() {
+        populateInfoWindow(this, infoWindow);
         map.panTo(this.getPosition());
+        toggleBounce(this);
     });
 
     // Two event listeners - one for mouseover, one for mouseout,
@@ -80,14 +89,56 @@ var Location = function(data) {
 var ViewModel = function() {
     var self = this;
 
+    // Knockout variables
     this.filterInput = ko.observable('');
     this.markerArray = ko.observableArray([]);
 
-    //Add all initial locations from the data model to the empty observable array
+    // Add all initial locations from the data model to the empty observable array
     locations.forEach(function(locationItem) {
         self.markerArray.push( new Location(locationItem) );
     });
+
+    // locations viewed on map
+    this.locationList = ko.computed(function() {
+        var filter = self.filterInput().toLowerCase();
+        if (filter) {
+            return ko.utils.arrayFilter(self.markerArray(), function(location) {
+                var str = location.title.toLowerCase();
+                var result = str.includes(filter);
+                location.visible(result);
+                return result;
+            });
+        } else {
+            self.markerArray().forEach(function(location) {
+                location.visible(true);
+            });
+        }
+        return self.markerArray();
+    }, self);
 };
+
+// This function populates the infowindow when the marker is clicked. We'll only allow
+// one infowindow which will open at the marker that is clicked, and populate based
+// on that markers position.
+function populateInfoWindow(marker, infowindow) {
+    // Check to make sure the infowindow is not already opened on this marker.
+    if (infowindow.marker != marker) {
+        // Clear the infowindow content to give the streetview time to load.
+        infowindow.setContent('');
+        infowindow.marker = marker;
+
+        // Make sure the marker property is cleared if the infowindow is closed.
+        infowindow.addListener('closeclick', function() {
+            infowindow.marker = null;
+        });
+
+        // Create the infowindow content for this marker
+        infowindow.setContent('<h4>' + marker.title + '</h4>');
+
+        // Open the infowindow on the correct marker.
+        infowindow.open(map, marker);
+    }
+}
 
 
 // This function takes in a COLOR, and then creates a new marker
@@ -102,4 +153,16 @@ function makeMarkerIcon(markerColor) {
         new google.maps.Point(10, 34),
         new google.maps.Size(21, 34));
     return markerImage;
+}
+
+// This function makes a marker on the map bounce
+function toggleBounce(marker) {
+    if (marker.getAnimation() !== null) {
+        marker.setAnimation(null);
+    } else {
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+        setTimeout(function() {
+            marker.setAnimation(null);
+        }, 2100);
+    }
 }
